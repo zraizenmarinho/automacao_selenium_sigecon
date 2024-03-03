93b4027de2443e899188cfcc994ec8759a6be5ed
zraizenmarinho <133124647+zraizenmarinho@users.noreply.github.com>
2024-03-02 22:08:13 -0300

    Update si_janeiro_mat.py

--- si_janeiro_mat.py
+++ si_janeiro_mat.py
@@ -54,143 +54,30 @@ def obter_matriculas_por_tipo(file_path):
     matriculas_por_tipo = MatriculasPorTipoFinanciamento(file_path)
 
 
-
-#Fevereiro
-
-    fev_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['2'], ['2023'], '1 Gratuidade Regimental')
-    fev_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['2'], ['2023'], '2 Gratuidade Não Regimental')
-    fev_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['2'], ['2023'], '3 Convênio')
-    fev_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['2'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Março
-
-    mar_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['3'], ['2023'], '1 Gratuidade Regimental')
-    mar_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['3'], ['2023'], '2 Gratuidade Não Regimental')
-    mar_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['3'], ['2023'], '3 Convênio')
-    mar_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['3'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Abril
-
-    abr_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['4'], ['2023'], '1 Gratuidade Regimental')
-    abr_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['4'], ['2023'], '2 Gratuidade Não Regimental')
-    abr_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['4'], ['2023'], '3 Convênio')
-    abr_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['4'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Maio
-
-    mai_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['5'], ['2023'], '1 Gratuidade Regimental')
-    mai_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['5'], ['2023'], '2 Gratuidade Não Regimental')
-    mai_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['5'], ['2023'], '3 Convênio')
-    mai_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['5'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-
-#Junho
-
-    jun_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['6'], ['2023'], '1 Gratuidade Regimental')
-    jun_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['6'], ['2023'], '2 Gratuidade Não Regimental')
-    jun_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['6'], ['2023'], '3 Convênio')
-    jun_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['6'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Julho
-
-    jul_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['7'], ['2023'], '1 Gratuidade Regimental')
-    jul_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['7'], ['2023'], '2 Gratuidade Não Regimental')
-    jul_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['7'], ['2023'], '3 Convênio')
-    jul_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['7'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Agosto
-
-    ago_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['8'], ['2023'], '1 Gratuidade Regimental')
-    ago_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['8'], ['2023'], '2 Gratuidade Não Regimental')
-    ago_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['8'], ['2023'], '3 Convênio')
-    ago_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['8'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-
-#Setembro
-
-    set_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['9'], ['2023'], '1 Gratuidade Regimental')
-    set_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['9'], ['2023'], '2 Gratuidade Não Regimental')
-    set_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['9'], ['2023'], '3 Convênio')
-    set_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['9'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-
-#Outubro
-
-    out_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['10'], ['2023'], '1 Gratuidade Regimental')
-    out_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['10'], ['2023'], '2 Gratuidade Não Regimental')
-    out_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['10'], ['2023'], '3 Convênio')
-    out_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['10'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-#Novembro
-
-    nov_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['11'], ['2023'], '1 Gratuidade Regimental')
-    nov_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['11'], ['2023'], '2 Gratuidade Não Regimental')
-    nov_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['11'], ['2023'], '3 Convênio')
-    nov_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['11'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-
-#Dezembro
-
-    dez_ip_mat_regi = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['12'], ['2023'], '1 Gratuidade Regimental')
-    dez_ip_mat_bolsa = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['12'], ['2023'], '2 Gratuidade Não Regimental')
-    dez_ip_mat_convenio = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['12'], ['2023'], '3 Convênio')
-    dez_ip_mat_n_gratuita = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', ['12'], ['2023'], '9 Pago por Pessoa Fisica ou Empresa')
-
-
-    return {
-    
-        "fev_ip_mat_regi": fev_ip_mat_regi,
-        "fev_ip_mat_bolsa":fev_ip_mat_bolsa,
-        "fev_ip_mat_convenio":fev_ip_mat_convenio,
-        "fev_ip_mat_n_gratuita":fev_ip_mat_n_gratuita,
-
-        "abr_ip_mat_regi": abr_ip_mat_regi,
-        "abr_ip_mat_bolsa":abr_ip_mat_bolsa,
-        "abr_ip_mat_convenio":abr_ip_mat_convenio,
-        "abr_ip_mat_n_gratuita":abr_ip_mat_n_gratuita,
-
-        "mai_ip_mat_regi": mai_ip_mat_regi,
-        "mai_ip_mat_bolsa":mai_ip_mat_bolsa,
-        "mai_ip_mat_convenio":mai_ip_mat_convenio,
-        "mai_ip_mat_n_gratuita":mai_ip_mat_n_gratuita,
-
-        "jun_ip_mat_regi": jun_ip_mat_regi,
-        "jun_ip_mat_bolsa":jun_ip_mat_bolsa,
-        "jun_ip_mat_convenio":jun_ip_mat_convenio,
-        "jun_ip_mat_n_gratuita":jun_ip_mat_n_gratuita,
-
-        "jul_ip_mat_regi": jul_ip_mat_regi,
-        "jul_ip_mat_bolsa":jul_ip_mat_bolsa,
-        "jul_ip_mat_convenio":jul_ip_mat_convenio,
-        "jul_ip_mat_n_gratuita":jul_ip_mat_n_gratuita,
-
-        "ago_ip_mat_regi": ago_ip_mat_regi,
-        "ago_ip_mat_bolsa":ago_ip_mat_bolsa,
-        "ago_ip_mat_convenio":ago_ip_mat_convenio,
-        "ago_ip_mat_n_gratuita":ago_ip_mat_n_gratuita,
-
-        "set_ip_mat_regi": set_ip_mat_regi,
-        "set_ip_mat_bolsa":set_ip_mat_bolsa,
-        "set_ip_mat_convenio":set_ip_mat_convenio,
-        "set_ip_mat_n_gratuita":set_ip_mat_n_gratuita,
-
-        "out_ip_mat_regi": out_ip_mat_regi,
-        "out_ip_mat_bolsa":out_ip_mat_bolsa,
-        "out_ip_mat_convenio":out_ip_mat_convenio,
-        "out_ip_mat_n_gratuita":out_ip_mat_n_gratuita,
-
-        "nov_ip_mat_regi": nov_ip_mat_regi,
-        "nov_ip_mat_bolsa":nov_ip_mat_bolsa,
-        "nov_ip_mat_convenio":nov_ip_mat_convenio,
-        "nov_ip_mat_n_gratuita":nov_ip_mat_n_gratuita,
-
-        "dez_ip_mat_regi": dez_ip_mat_regi,
-        "dez_ip_mat_bolsa":dez_ip_mat_bolsa,
-        "dez_ip_mat_convenio":dez_ip_mat_convenio,
-        "dez_ip_mat_n_gratuita":dez_ip_mat_n_gratuita, 
+    meses = {
+ 
+        'fev': ['22024'],
+        'mar': ['32024'],
+        'abr': ['42024'],
+        'mai': ['52024'],
+        'jun': ['62024'],
+        'jul': ['72024'],
+        'ago': ['82024'],
+        'set': ['92024'],
+        'out': ['102024'],
+        'nov': ['112024'],
+        'dez': ['122024']
     }
-
-
+    
+    resultados_mat = {}
+    
+    for mes_atual, mes_referencia in meses.items():
+        for tipo_financiamento in ['1 Gratuidade Regimental', '2 Gratuidade Não Regimental', '3 Convênio', '9 Pago por Pessoa Fisica ou Empresa']:
+            chave_resultado = f"{mes_atual}_ip_mat_{tipo_financiamento}"
+            resultados[chave_resultado] = matriculas_por_tipo.contar_matriculas('1117376 SENAI Taguatinga', '5 Iniciação Profissional', '1 Presencial', mes_referencia, tipo_financiamento)
+    
+    return resultados_mat
+    
 def obter_hora_por_tipo(file_path):
     class HorasPorTipoFinanciamento:
         def __init__(self, file_path):
@@ -229,7 +116,7 @@ def obter_hora_por_tipo(file_path):
     }
 
     # Inicializando um dicionário para armazenar os resultados
-    resultados = {}
+    resultados_ha = {}
 
     # Iterando sobre os meses
     for mes_atual, mes_referencia in meses.items():
@@ -253,7 +140,7 @@ def obter_hora_por_tipo(file_path):
             soma_meses_anteriores = sum(resultados[f"{list(meses.keys())[i]}_ip_ha_{tipo_financiamento}"] for i in range(idx))
             resultados[chave_resultado_atual] -= soma_meses_anteriores
 
-    return resultados
+    return resultados_ha
 
 def obter_concluintes_por_tipo(file_path):
     class ConcluintesPorTipoFinanciamento:
